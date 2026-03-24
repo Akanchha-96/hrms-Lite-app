970d9ba99c14232337053f066fbee7eaa671dcbb
Akanksha Jha <akanchhajha96@gmail.com>
2026-03-24 19:35:47 +0530

    modified app.py

--- frontend/src/components/AttendanceForm.js
+++ frontend/src/components/AttendanceForm.js
@@ -1,44 +1,36 @@
-import { useState } from "react";
+import {useState} from "react";
 import { markAttendance } from "../api";
 
-function AttendanceForm() {
-    const [data, setData] = useState({
-        employee_id: "",
-        date: "",
-        status: "Present"
+function AttendanceForm(){
+    const [data,setData]=useState({
+        employee_id:"",
+        date:"",
+        status:"Present"
     });
-
-    const handleSubmit = async (e) => {
+    const handleSubmit=async(e)=>{
         e.preventDefault();
-        try {
-            const res = await markAttendance(data);
-            alert(res.data.message);
-        } catch (err) {
-            if (err.response) {
-                alert(err.response.data.error);
-            } else {
-                alert("Network error or CORS issue");
-            }
-        }
+        await markAttendance(data);
     };
-
-    return (
+    return(
         <form onSubmit={handleSubmit}>
+            <input placeholder="Employee ID" 
+            onChange={e=>setData({...data,employee_id:e.target.value})}/>
+            
             <input 
-                placeholder="Employee ID" 
-                onChange={e => setData({...data, employee_id: e.target.value})}
-            />
-            <input 
-                placeholder="Date" 
-                type="date"
-                onChange={e => setData({...data, date: e.target.value})}
-            />
-            <select onChange={e => setData({...data, status: e.target.value})}>
-                <option>Present</option>
-                <option>Absent</option>
+            type="date"
+            placeholder="Date" 
+            onChange={e=>setData({...data,date:e.target.value})}/>
+
+            <select 
+            onChange={e=>setData({...data,status:e.target.value})}>
+            <option>Present</option>
+            <option>Absent</option>
             </select>
+            
+
             <button>Mark Attendance</button>
         </form>
-    );
+    )
 }
+
 export default AttendanceForm;
