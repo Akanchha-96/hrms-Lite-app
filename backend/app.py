424440912c16ab8481c81d3ff49bc0376cac42e8
Akanksha Jha <akanchhajha96@gmail.com>
2026-03-24 18:06:19 +0530

    modified api.js

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from employee_routes import employee_bp
 from attendance_routes import attendance_bp
 
 app=Flask(__name__)
-CORS(app)
+CORS(app, origins=["*"], supports_credentials=True)
 
 app.register_blueprint(employee_bp)
 app.register_blueprint(attendance_bp)
